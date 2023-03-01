51b1d9b82bc7a33b4452afdce0ab3ab6cfea71fc
sjursaevik <sjur.saevik@nasjonalmuseet.no>
2023-03-01 15:54:25 +0100

    Signed-off-by: sjursaevik <sjur.saevik@nasjonalmuseet.no>

--- app_nel.py
+++ app_nel.py
@@ -24,10 +24,9 @@ with col1:
                         #Gjennom grafiske reproduksjoner fikk Brudeferd i Hardanger stor utbredelse, og på grunn av motivets spesielle popularitet har kunstnerne utført maleriet i flere versjoner. Adolph Tidemand var den første norske kunstner som slo seg ned i Düsseldorf. Sin ambisjon om å bli historiemaler oppga han for å bli folkelivsskildrer. Men Tidemand gir en ny verdighet til bøndene, og dikteren Bjørnstjerne Bjørnson skal ha sagt at uten Tidemands malerier hadde han ikke kunnet skrive sine bondefortellinger.
 
                         #Landskapsmaler Hans Gude, som var drøye ti år yngre enn Tidemand, presenterer her som 23-åring en storslagen skildring av norsk natur. Selv om det ikke dreier seg om en direkte gjengivelse av et bestemt landskap, er komposisjonen satt sammen av nøyaktige naturobservasjoner fra forskjellige steder i hjemlandet. Tidemand og Gude har utført flere malerier sammen, der alle motivene viser folk som er ute i båt.''', max_chars=1000000)
-
+    doc=makedoc(text)
     if st.button("analyser tekst!", ):
         st.write("analyserer tekst...")
-        doc=makedoc(text)
         st.write("ferdig!")
 
         with col2:
